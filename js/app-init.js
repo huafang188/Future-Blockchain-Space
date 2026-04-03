e0cbccf454c27f09140f4534a864837163695664
huafang188 <d20240915@qq.com>
2026-04-03 22:18:17 +0800

    Update app-init.js

--- js/app-init.js
+++ js/app-init.js
@@ -1,15 +1,19 @@
 import { mountWalletClickHandler, connectWallet, resetWalletUI, updateWalletUI, setCurrentAddress } from './wallet-utils.js';
 import { fetchUserData, updateText } from './api-service.js';
-import { renderTokenList, renderHistory, renderTransfers, renderStatsPage } from './ui-render.js';
+import { renderTokenList, renderHistory, renderTransfers, renderStatsPage, renderNews } from './ui-render.js';
 import { mountModalHandlers } from './modal-handler.js';
 import { mountCalculationHandlers } from './calculations.js';
 import { mountActionExecutors } from './action-executor.js';
 
+// 全局挂载（修复所有找不到函数的问题）
 window.updateText = updateText;
+window.renderTokenList = renderTokenList;
 window.renderHistory = renderHistory;
 window.renderTransfers = renderTransfers;
-window.renderStatsPage = renderStatsPage; 
+window.renderStatsPage = renderStatsPage;
+window.renderNews = renderNews;
 
+// 初始化
 function initApp() {
     mountWalletClickHandler();
     mountModalHandlers();
@@ -18,14 +22,16 @@ function initApp() {
 
     window.connectWallet = connectWallet;
     window.fetchUserData = fetchUserData;
-    window.renderTokenList = renderTokenList;
 
     window.onload = () => {
+        renderNews();
+        renderStatsPage();
+        renderTokenList({});
+
         if (window.i18nRender) window.i18nRender();
 
         const currentAddress = localStorage.getItem('fbs_address');
         const isManualLogout = localStorage.getItem('user_logout_manual');
-        
         setCurrentAddress(currentAddress);
 
         if (currentAddress && isManualLogout !== 'true') {
@@ -34,10 +40,6 @@ function initApp() {
         } else {
             resetWalletUI();
         }
-        
-        renderTokenList({});
-        renderStatsPage(localStorage.getItem('fbs_lang') || 'zh-CN');
-        if (window.i18nRender) window.i18nRender();
     };
 }
 
